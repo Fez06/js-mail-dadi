eef28fcbe96c1f3b8e04e046cb357fb305d45700
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-24 14:59:38 +0100

    prima parte fatta, manca il messaggio di errore

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,20 @@
 'use script';
+
+const request = prompt('Inserisci il tuo indirizzo email per accedere:');
+
+const rubricaEmail = ['matteo@GainNode.it', 'caino@abele.com', 'andrea@lino.it', 'rarod@dif.com', 'glenn@sturgis.com'];
+
+for (let i = 0; i < rubricaEmail.length; i++) {
+    console.log(rubricaEmail[i]);
+
+    
+    if ( rubricaEmail[i] === request ) {
+
+        alert('benvenuto nel nostro servizio');
+        break;
+
+    } 
+    
+    
+
+};
