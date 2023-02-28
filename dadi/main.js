aebf8dd2d3d1de87a372dc6ad5210c8ce3bb65e8
Fez06 <mattiafezzardi1993@gmail.com>
2023-02-28 18:27:39 +0100

    finito

--- dadi/main.js
+++ dadi/main.js
@@ -1,1 +1,21 @@
 'use script';
+
+const btn = document.querySelector('.btn');
+let valore1 = '';
+let valore2 = '';
+
+btn.addEventListener('click', function(){
+    valore1 = Math.floor((Math.random() * 6) + 1);
+    console.log('valore1');
+
+    valore2 = Math.floor((Math.random() * 6) + 1);
+    console.log('valore1');
+
+    if (valore1 > valore2) {
+        alert('hai vinto');
+    } else if (valore1 < valore2) {
+        alert('hai perso');
+    } else { 
+        alert('pareggio');
+    }
+})
